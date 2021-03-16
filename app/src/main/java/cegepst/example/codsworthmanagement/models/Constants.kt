01290f2bc0d53fea8b9fd6e2c8475ce1588aa383
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2021-03-16 16:24:07 -0400

    made constants for the app

--- app/src/main/java/cegepst/example/codsworthmanagement/models/Constants.kt
+++ app/src/main/java/cegepst/example/codsworthmanagement/models/Constants.kt
@@ -1,3 +1,25 @@
 package cegepst.example.codsworthmanagement.models
 
-class Constants
+class Constants {
+
+    val maxAmelioration: Int = 18
+    val timeDecreaseRatio: Double = 0.05
+
+    val waterInitialCost: Int = 0
+    val waterModificationPrice: Int = 25
+    val waterMrHandyPrice: Int = 1000
+    val waterRevenue: Int = 5
+    val waterProductionTime: Int = 2
+
+    val steakInitialPrice: Int = 1000
+    val steakModificationPrice: Int = 250
+    val steakMrHandyPrice: Int = 5000
+    val steakRevenue: Int = 50
+    val steakProductionTime: Int = 10
+
+    val colaInitialCost: Int = 10000
+    val colaModificationCost: Int = 10000
+    val colaMrHandyPrice: Int = 25000
+    val colaRevenue: Int = 500
+    val colaProductionTime: Int = 30
+}
